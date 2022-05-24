969d7f1de300a22e9f66c42527bab4ecbc5325a1
r-cha <robert@akchandler.com>
2022-05-23 21:42:06 -0700

    Separate model files

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    """Only the basic metadata of a user"""
+    name: str
+    email: str
+    # etc...
+
+
+class Applicant(User):
+    """Details of the user as a persona"""
+    resume: str  # actually a document somewhere
+    industry: str  # probably another table or enum
+    # etc...
